f6abcc2f1424b47be569940142e0b110b5c0e613
bulk88 <bulk88@hotmail.com>
2015-03-18 00:39:57 -0400

    update canned microsft/config.h from autoconf generated config.h
    done with cygwin autoconf, no gcc installed, vcvars32.bat env vars set from
    VC 6, macros that were undefed were not included in this commit, also
    autoconf undefed HAVE_UTIME_NULL, so leave the former hand set
    HAVE_UTIME_NULL on.

--- win95/microsft/config.h
+++ win95/microsft/config.h
@@ -34,11 +34,6 @@
    Don't forget to update the PACKAGE and VERSION macros!
 */
 
-/* Name and version number of this package */
-#define PACKAGE "dmake"
-#define VERSION "4.13"
-#define BUILDINFO "Windows / MS Visual C++ " dmstr2(_MSC_FULL_VER)
-
 #if defined (_MSC_VER)
 # if _MSC_VER < 500
 	Force a compile-time blowup.
@@ -83,33 +78,104 @@
 #   define CONST
 #endif
 
+/* Build info string */
+#define BUILDINFO "Windows / MS Visual C++ " dmstr2(_MSC_FULL_VER)
+
 /* Assume case insensitive file system. */
 #define CASE_INSENSITIVE_FS 1
 
-/* MSC has sys/types.h and sys/stat.h (this is tested only with MSVC++ 6.0) */
-#define HAVE_SYS_TYPES_H 1
-#define HAVE_SYS_STAT_H 1
 
 /* These functions are available! (this is tested only with MSVC++ 6.0) */
-#define HAVE_GETCWD 1
 #define HAVE_UTIME_NULL 1
-#define HAVE_TZSET 1
-#define HAVE_STRLWR 1
 
+/* Define to 1 if you have the <errno.h> header file. */
 #define HAVE_ERRNO_H 1
-#define HAVE_STRERROR 1
-#define HAVE_TEMPNAM 1
+
+/* Define to 1 if you have the <fcntl.h> header file. */
 #define HAVE_FCNTL_H 1
+
+/* Define to 1 if you have the `getcwd' function. */
+#define HAVE_GETCWD 1
+
+/* Define to 1 if you have the <limits.h> header file. */
+#define HAVE_LIMITS_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `setvbuf' function. */
 #define HAVE_SETVBUF 1
 
 /* These defines are needed for itypes.h! (this is tested only with MSVC++ 6.0) */
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the `strerror' function. */
+#define HAVE_STRERROR 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strlwr' function. */
+#define HAVE_STRLWR 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the `tempnam' function. */
+#define HAVE_TEMPNAM 1
+
+/* Define to 1 if you have the `tzset' function. */
+#define HAVE_TZSET 1
+
+/* Define to 1 if you have the `vprintf' function. */
+#define HAVE_VPRINTF 1
+
+/* Name of package */
+#define PACKAGE "dmake"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "dmake"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "dmake 4.13"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "dmake"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "4.13"
+
+/* Define as the return type of signal handlers (`int' or `void'). */
+#define RETSIGTYPE void
+
+/* The size of `int', as computed by sizeof. */
 #define SIZEOF_INT 4
+
+/* The size of `long', as computed by sizeof. */
 #define SIZEOF_LONG 4
+
+/* The size of `short', as computed by sizeof. */
 #define SIZEOF_SHORT 2
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
 
 #ifndef MSDOS
 #   define MSDOS 1
 #endif
+
+/* Version number of package */
+#define VERSION "4.13"
 
 /* a small problem with pointer to voids on some unix machines needs this */
 #define DMPVOID void *
